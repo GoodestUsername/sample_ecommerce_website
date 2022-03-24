d67d8d83617755b8c4b37283c09531931df959d6
GoodestUsername <ericdongcannn@hotmail.com>
2022-03-23 17:15:03 -0700

    added remove and add one item functionality to cart controller

--- sample_ecommerce_website/Controllers/ShoppingCartController.cs
+++ sample_ecommerce_website/Controllers/ShoppingCartController.cs
@@ -18,7 +18,6 @@ namespace sample_ecommerce_website.Controllers
         private readonly ProductDBModel _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public 
         public ShoppingCartController(UserManager<ApplicationUser> userManager, 
             ProductDBModel context)
         {
@@ -58,13 +57,14 @@ namespace sample_ecommerce_website.Controllers
                     Quantity = 1,
                     DateCreated = DateTime.Now
                 };
-                user.ShoppingCart.CartItems.Add(newCartItem);
+               cart.Add(newCartItem);
             }
             else
             {
                 cart[index].Quantity += 1;
-                user.ShoppingCart.CartItems = cart;
             }
+
+            user.ShoppingCart.CartItems = cart;
             user.ShoppingCart.ItemQuantity += 1;
             user.ShoppingCart.Total += _context.Products.Find(itemId).Price;
             await _userManager.UpdateAsync(user);
@@ -72,7 +72,7 @@ namespace sample_ecommerce_website.Controllers
             return View("Index", user.ShoppingCart.CartItems.ToList());
         }
 
-        public async Task<IActionResult> RemoveOneFromCart(string itemId)
+        public async Task<IActionResult> RemoveOneFromCart(string CartItemId)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -81,17 +81,62 @@ namespace sample_ecommerce_website.Controllers
             }
             List<CartItem> cart = user.ShoppingCart.CartItems.ToList();
 
-            int index = cart.FindIndex(item => item.ProductId == itemId);
+            int index = cart.FindIndex(item => item.CartItemId == CartItemId);
+            string ProductID = cart[index].ProductId;
+
+            int OldQuantity = cart[index].Quantity;
 
             cart[index].Quantity -= 1;
+
             if (cart[index].Quantity == 0)
             {
                 cart.RemoveAt(index);
             }
+
+            user.ShoppingCart.Total -= _context.Products.Find(ProductID).Price;
+            user.ShoppingCart.ItemQuantity -= 1;
             user.ShoppingCart.CartItems = cart;
 
-            user.ShoppingCart.ItemQuantity -= 1;
-            user.ShoppingCart.Total -= _context.Products.Find(itemId).Price;
+            await _userManager.UpdateAsync(user);
+
+            return View("Index", user.ShoppingCart.CartItems.ToList());
+        }
+
+        public async Task<IActionResult> ChangeItemQuantityInCart(string CartItemId, int NewQuantity)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+            List<CartItem> cart = user.ShoppingCart.CartItems.ToList();
+
+            int index = cart.FindIndex(item => item.CartItemId == CartItemId);
+            string ProductID = cart[index].ProductId;
+
+            int OldQuantity = cart[index].Quantity;
+
+            cart[index].Quantity = NewQuantity;
+
+            if (OldQuantity > NewQuantity)
+            {
+                user.ShoppingCart.Total -= _context.Products.Find(ProductID).Price * NewQuantity;
+                user.ShoppingCart.ItemQuantity -= NewQuantity;
+            }
+            else if (OldQuantity == NewQuantity) {}
+            else
+            {
+                user.ShoppingCart.Total += _context.Products.Find(ProductID).Price * NewQuantity;
+                user.ShoppingCart.ItemQuantity += NewQuantity;
+            }
+
+            if (cart[index].Quantity == 0)
+            {
+                cart.RemoveAt(index);
+            }
+
+            user.ShoppingCart.CartItems = cart;
+
             await _userManager.UpdateAsync(user);
 
             return View("Index", user.ShoppingCart.CartItems.ToList());
@@ -118,7 +163,7 @@ namespace sample_ecommerce_website.Controllers
             return View("Index", user.ShoppingCart.CartItems.ToList());
         }
 
-        public async Task<IActionResult> EmptyFromCart()
+        public async Task<IActionResult> EmptyCart()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
